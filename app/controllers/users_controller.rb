032e759aa62b809d2732f684c479e8394bb8b9a4
shady-mungai <shadrackmungai10@gmail.com>
2023-03-19 04:54:50 +0300

    Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+    def accepted_params
+        params.permit(:id, :username, :city)
+    end
+
 end
